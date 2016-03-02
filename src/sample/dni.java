832ec3381e86f788f87795a37e951f8ffe44e048
fjcambilr <xcambil@gmail.com>
2016-03-02 20:58:00 +0100

    Initial commit

---
+++ src/sample/dni.java
@@ -0,0 +1,13 @@
+package sample;
+
+import com.esliceu.ProvaCaixaNegre;
+
+/**
+ * Created by fjcambilr on 11/02/16.
+ */
+public class dni {
+    public static void main(String[] args) {
+
+        ProvaCaixaNegre p = new ProvaCaixaNegre(43171235);
+    }
+}
